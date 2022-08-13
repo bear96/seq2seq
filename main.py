3f4dd2c6144dc8b57080120b163850335e4584fb
bear96 <73417041+bear96@users.noreply.github.com>
2022-08-13 18:28:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,39 +8,26 @@ from collator import Collater
 from tqdm import tqdm
 from sklearn import model_selection
 import pickle
+from matplotlib import pyplot as plt
+import model
+import argparse
 
-f_vocab, expans_vocab,factors,expansions = process_dataset.preparevocab('Factors','Expansions', 'train.txt')
-dataset = pd.DataFrame({'factors': factors,'expansions':expansions})
 
-train_data,val_data = model_selection.train_test_split(dataset,train_size=0.9,random_state=44)
-test_data,val_data = model_selection.train_test_split(val_data,train_size=0.2,random_state=44)
-
-train_dataset = process_dataset.PolyData(f_vocab,expans_vocab,train_data.factors.to_numpy(),train_data.expansions.to_numpy())
-val_dataset = process_dataset.PolyData(f_vocab,expans_vocab,val_data.factors.to_numpy(),val_data.expansions.to_numpy())
-
-test_dataset = process_dataset.PolyData(f_vocab,expans_vocab,test_data.factors.to_numpy(),test_data.expansions.to_numpy())
-
-if torch.cuda.is_available():
-  device = 'cuda'
-else:
-  device = 'cpu'
-
-print("Device: ",device)
-
-SRC_VOCAB_SIZE = f_vocab.n_words
-TGT_VOCAB_SIZE = expans_vocab.n_words
-EMB_SIZE = 256
-NHEAD = 8
-FFN_HID_DIM = 512
-BATCH_SIZE = 128
-NUM_ENCODER_LAYERS = 3
-NUM_DECODER_LAYERS = 3
-collate_fn = Collater(f_vocab, expans_vocab)
-
-def train(train_dataset,val_dataset,SRC_VOCAB_SIZE,SRC_VOCAB_SIZE,EMB_SIZE,NHEAD,FFN_HID_DIM,BATCH_SIZE,NUM_ENCODER_LAYERS,NUM_DECODER_LAYERS,collate_fn):
+def train(train_data,val_data,f_vocab,expans_vocab,collate_fn,args):
+  SRC_VOCAB_SIZE = f_vocab.n_words
+  TGT_VOCAB_SIZE = expans_vocab.n_words
+  if torch.cuda.is_available():
+    device = 'cuda'
+  else:
+    device = 'cpu'
+    
+  print("Device: ",device)
   torch.manual_seed(0)
-  transformer = Seq2SeqTransformer(NUM_ENCODER_LAYERS, NUM_DECODER_LAYERS, EMB_SIZE,
-                                 NHEAD, SRC_VOCAB_SIZE, TGT_VOCAB_SIZE, FFN_HID_DIM)
+  print("Processing training dataset...")
+  train_dataset = process_dataset.PolyData(f_vocab,expans_vocab,train_data.factors.to_numpy(),train_data.expansions.to_numpy())
+  print("Processing validation dataset...")
+  val_dataset = process_dataset.PolyData(f_vocab,expans_vocab,val_data.factors.to_numpy(),val_data.expansions.to_numpy())
+  transformer = model.Seq2SeqTransformer(SRC_VOCAB_SIZE,TGT_VOCAB_SIZE,args.n_enc,args.n_dec, args.embedding_size,args.n_head,args.ffn)
 
   pytorch_total_params = sum(p.numel() for p in transformer.parameters() if p.requires_grad)
   print("Total trainable params: ",pytorch_total_params)
@@ -61,11 +48,11 @@ def train(train_dataset,val_dataset,SRC_VOCAB_SIZE,SRC_VOCAB_SIZE,EMB_SIZE,NHEAD
   loss_fn = torch.nn.CrossEntropyLoss(ignore_index=2)
   optimizer = torch.optim.Adam(transformer.parameters(), lr=0.00005)
   
-  train_dataloader = DataLoader(train_dataset,batch_size=128,shuffle=True,collate_fn=collate_fn)
-  val_dataloader = DataLoader(val_dataset,batch_size=128,shuffle=False,collate_fn=collate_fn)
+  train_dataloader = DataLoader(train_dataset,batch_size=args.batch_size,shuffle=True,collate_fn=collate_fn)
+  val_dataloader = DataLoader(val_dataset,batch_size=args.batch_size,shuffle=False,collate_fn=collate_fn)
   train_loss = []
   valid_loss = []
-  for i in range(1):
+  for i in range(args.epochs):
     transformer.train()
     tr_loss = 0.0
     val_loss =0.0
@@ -75,7 +62,7 @@ def train(train_dataset,val_dataset,SRC_VOCAB_SIZE,SRC_VOCAB_SIZE,EMB_SIZE,NHEAD
 
       tgt_input = tgt[:-1, :]
 
-      src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, tgt_input)
+      src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = model.create_mask(src, tgt_input)
 
       logits = transformer(src, tgt_input, src_mask, tgt_mask,src_padding_mask, tgt_padding_mask, src_padding_mask)
       torch.nn.utils.clip_grad_norm_(transformer.parameters(), max_norm=1)
@@ -96,7 +83,7 @@ def train(train_dataset,val_dataset,SRC_VOCAB_SIZE,SRC_VOCAB_SIZE,EMB_SIZE,NHEAD
 
       tgt_input = tgt[:-1, :]
 
-      src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, tgt_input)
+      src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = model.create_mask(src, tgt_input)
 
       logits = transformer(src, tgt_input, src_mask, tgt_mask,src_padding_mask, tgt_padding_mask, src_padding_mask)
 
@@ -124,10 +111,21 @@ def train(train_dataset,val_dataset,SRC_VOCAB_SIZE,SRC_VOCAB_SIZE,EMB_SIZE,NHEAD
 
   torch.save(transformer.state_dict(), PATH)
   
-def test(test_dataset,SRC_VOCAB_SIZE,SRC_VOCAB_SIZE,EMB_SIZE,NHEAD,FFN_HID_DIM,BATCH_SIZE,NUM_ENCODER_LAYERS,NUM_DECODER_LAYERS,collate_fn):
+def test(test_data,f_vocab,expans_vocab,collate_fn,args):
+
+  SRC_VOCAB_SIZE = f_vocab.n_words
+  TGT_VOCAB_SIZE = expans_vocab.n_words
+  if torch.cuda.is_available():
+    device = 'cuda'
+  else:
+    device = 'cpu'
+  
+  print("Device: ",device)
+  
+  print("Processing test dataset...")
+  test_dataset = process_dataset.PolyData(f_vocab,expans_vocab,test_data.factors.to_numpy(),test_data.expansions.to_numpy())
   torch.manual_seed(0)
-  transformer = Seq2SeqTransformer(NUM_ENCODER_LAYERS, NUM_DECODER_LAYERS, EMB_SIZE,
-                                 NHEAD, SRC_VOCAB_SIZE, TGT_VOCAB_SIZE, FFN_HID_DIM)
+  transformer = model.Seq2SeqTransformer(SRC_VOCAB_SIZE,TGT_VOCAB_SIZE,args.n_enc,args.n_dec, args.embedding_size,args.n_head,args.ffn).to(device)
   transformer.load_state_dict(torch.load('transformer_model.pkl'))
   print("Testing...")
   test_dataloader = DataLoader(test_dataset,batch_size=1,shuffle=False,collate_fn=collate_fn)
@@ -145,11 +143,11 @@ def test(test_dataset,SRC_VOCAB_SIZE,SRC_VOCAB_SIZE,EMB_SIZE,NHEAD,FFN_HID_DIM,B
     ys = torch.ones(1, 1).fill_(0).type(torch.long).to(device)
     for i in range(29):
       memory = memory.to(device)
-      tgt_mask = (generate_square_subsequent_mask(ys.size(0))
+      tgt_mask = (model.generate_square_subsequent_mask(ys.size(0))
                     .type(torch.bool)).to(device)
       out = transformer.decode(ys, memory, tgt_mask)
       out = out.transpose(0, 1)
-      prob = transformer.generator(out[:, -1])
+      prob = transformer.fc(out[:, -1])
       _, next_word = torch.max(prob, dim=1)
       next_word = next_word.item()
 
@@ -158,9 +156,8 @@ def test(test_dataset,SRC_VOCAB_SIZE,SRC_VOCAB_SIZE,EMB_SIZE,NHEAD,FFN_HID_DIM,B
         break
         
     pred_expansions.append(ys.cpu().numpy().reshape(-1))
-    
-    preds = decode_token_ids(pred_expansions)
-    return preds
+  preds = decode_token_ids(pred_expansions)
+  return preds
  
 def decode_token_ids(pred_expansions):
   preds = []
@@ -177,7 +174,7 @@ def decode_token_ids(pred_expansions):
     preds.append(word)
   s,actuals = get_score(test_data,preds)
   print("Saving...")
-  d = {'Score':c/len(preds),'Preds':preds,'Actuals': list(actuals)}
+  d = {'Score':s,'Preds':preds,'Actuals': list(actuals)}
   with open('preds.pkl', 'wb') as f:
     pickle.dump(d, f)
   
@@ -189,6 +186,7 @@ def score(true_expansion: str, pred_expansion: str) -> int:
   :return:
   """
   return int(true_expansion == pred_expansion)
+  
 def get_score(test_data,preds):
   actuals = test_data.expansions.to_numpy()
   c=0
@@ -196,8 +194,36 @@ def get_score(test_data,preds):
     c+=score(actuals[i],preds[i])
   print("Score: ",c/len(preds))
   return c/len(preds),actuals
+  
+  
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--data_dir", default='train.txt', type=str, help="Directory of the dataset for training.")
+  parser.add_argument("--blocksize", default = 30, type = int, help = "Maximum length of sequence. Default = 30")
+  parser.add_argument("--batch_size", default = 128, type = int, help = "Batch size for training and validation. Default = 32")
+  parser.add_argument("--epoch", default = 20, type = int, help = "Number of epochs for training. Default = 10.")
+  parser.add_argument("--do_train", action ='store_true',default= False, help = "Use if training.")
+  parser.add_argument("--do_test", action = 'store_true', default= False, help = "Use if testing.")
+  parser.add_argument("--embedding_size", default= 256,type=int, help="Embedding size used in transformer.")
+  parser.add_argument("--n_head", default = 8, type = int, help = "Number of heads for the attention model.")
+  parser.add_argument("--ffn", default= 512, type = int, help = "Size of the feedforward networks in transformer.")
+  parser.add_argument("--n_enc" , default = 3, type = int, help = "Number of encoder layers in transformer.")
+  parser.add_argument("--n_dec", default = 3, type = int, help = "Number of decoder layers in transformer.")
+    
+  args = parser.parse_args()
+  print(args)
+  f_vocab, expans_vocab,factors,expansions = process_dataset.preparevocab('Factors','Expansions', args.data_dir)
+  dataset = pd.DataFrame({'factors': factors,'expansions':expansions})
 
-
-train()
-test()
-
+  train_data,val_data = model_selection.train_test_split(dataset,train_size=0.9,random_state=44)
+  test_data,val_data = model_selection.train_test_split(val_data,train_size=0.2,random_state=44)
+  
+  collate_fn = Collater(f_vocab, expans_vocab)
+ 
+  if args.do_train:
+    train(train_data,val_data,f_vocab,expans_vocab,collate_fn,args)
+  if args.do_test:
+    preds = test(test_data,f_vocab,expans_vocab,collate_fn,args)
+       
+if __name__ == "__main__":
+    main()
